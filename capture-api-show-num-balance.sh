291423c5c891bfda252409d8cb852e93bfc0218b
Bofu Chen (bafu) <bofu@numbersprotocol.io>
2023-05-18 15:54:40 +0800

    fix(capture-api-show-num-balance.sh): use higher performance API endpoint
    Signed-off-by: Bofu Chen (bafu) <bofu@numbersprotocol.io>

--- capture-api-show-num-balance.sh
+++ capture-api-show-num-balance.sh
@@ -6,5 +6,5 @@ setCaptureToken
 
 echo -e "\nYour Capture wallet balance:\n"
 
-curl -X GET "https://api.numbersprotocol.io/api/v3/auth/users/?show_num_balance=true" \
-     -H "Authorization: token ${captureToken}" | jq '.results[0].user_wallet'
+curl -X GET "https://api.numbersprotocol.io/api/v3/auth/users/me/" \
+     -H "Authorization: token ${captureToken}" | jq '.'
