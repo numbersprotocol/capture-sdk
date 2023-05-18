804a4cd1e6e9563272226c33f883098f64b2cd06
Bofu Chen (bafu) <bofu@numbersprotocol.io>
2023-05-18 16:19:54 +0800

    fix(capture-api-show-num-balance.sh): strip and beautify the user wallet info
    Signed-off-by: Bofu Chen (bafu) <bofu@numbersprotocol.io>

--- capture-api-show-num-balance.sh
+++ capture-api-show-num-balance.sh
@@ -6,5 +6,5 @@ setCaptureToken
 
 echo -e "\nYour Capture wallet balance:\n"
 
-curl -X GET "https://api.numbersprotocol.io/api/v3/auth/users/me/" \
-     -H "Authorization: token ${captureToken}" | jq '.'
+curl -X GET "https://api.numbersprotocol.io/api/v3/auth/users/me/?show_num_balance" \
+     -H "Authorization: token ${captureToken}" | jq '.user_wallet'
