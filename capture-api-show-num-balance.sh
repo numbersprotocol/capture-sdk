7285552751be7d72f3cbfc2f8784c7b72dec615e
Bofu Chen (bafu) <bofu@numbersprotocol.io>
2023-05-18 16:23:36 +0800

    fix(capture-api-show-num-balance.sh): show not only Capture Credits but also NUM balances
    Signed-off-by: Bofu Chen (bafu) <bofu@numbersprotocol.io>

--- capture-api-show-num-balance.sh
+++ capture-api-show-num-balance.sh
@@ -6,5 +6,5 @@ setCaptureToken
 
 echo -e "\nYour Capture wallet balance:\n"
 
-curl -X GET "https://api.numbersprotocol.io/api/v3/auth/users/me/?show_num_balance" \
+curl -X GET "https://api.numbersprotocol.io/api/v3/auth/users/me/?show_num_balance=true" \
      -H "Authorization: token ${captureToken}" | jq '.user_wallet'
