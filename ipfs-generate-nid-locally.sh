74a79f0b70e43531e31d35d958b6ff17e2d3a7b3
Bofu Chen (bafu) <bofu@numbersprotocol.io>
2023-05-25 20:15:31 +0800

    feat(ipfs-generate-nid-locally.sh): generate Nid locally with IPFS node
    Signed-off-by: Bofu Chen (bafu) <bofu@numbersprotocol.io>

---
+++ ipfs-generate-nid-locally.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -p "Filepath of the asset for generating Nid locally: " filePath
+if [ "${filePath}" == "" ]; then
+    echo "No filepath provided. Exiting..."
+    exit 1
+else
+    echo -e "\n\nGenerating Nid of ${filePath}...\n"
+fi
+
+# Capture output of 'ipfs add' command into a variable
+output=$(ipfs add --only-hash --cid-version=1 --quiet ${filePath})
+
+# Extract the CID from the output
+cid=$(echo $output | awk '{print $1}')
+
+# Print the Nid
+echo "Nid: ${cid}"
