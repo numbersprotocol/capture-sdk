9afea30eee5efcc9414103e8d1844b2f2d888b84
Bofu Chen (bafu) <bofu@numbersprotocol.io>
2023-07-27 19:20:46 +0800

    feat: add capture-api-sign-up-by-email.sh
    Signed-off-by: Bofu Chen (bafu) <bofu@numbersprotocol.io>

---
+++ capture-api-sign-up-by-email.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+read -p "Capture account email: " captureEmail
+read -p "Capture account username: " captureUsername
+read -sp "Password: " capturePassword
+read -sp "X-Api-Key: " captureApiKey
+echo "\n\nYour Capture accout sign up result is: "
+
+curl -X POST "https://dia-backend.numbersprotocol.io/api/v3/auth/users/" \
+     -H "Content-Type: application/json" \
+     -H "Accept: application/json" \
+     -H "X-Api-Key: ${captureApiKey}" \
+     -d "{
+        \"username\": \"${captureUsername}\",
+        \"password\": \"${capturePassword}\",
+        \"email\": \"${captureEmail}\",
+        \"activation_method\": \"skip\",
+        \"language\": \"en\"
+    }"
