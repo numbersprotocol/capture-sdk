afadccadf12eb60e7347611c461ca9ecc2e24f65
Bofu Chen (bafu) <bofu@numbersprotocol.io>
2025-02-22 18:06:20 +0800

    feat(capture-api-unregister-asset.sh): trivial update for user prompt
    Signed-off-by: Bofu Chen (bafu) <bofu@numbersprotocol.io>

--- capture-api-unregister-asset.sh
+++ capture-api-unregister-asset.sh
@@ -4,7 +4,7 @@ source utils.sh
 
 setCaptureToken
 
-read -p "Target Asset Nid: " assetNid
+read -p "The Nid of the asset you want to unregister: " assetNid
 echo -e "\n\nYour target Asset Nid for unregistration is: ${assetNid}"
 
 curl -X DELETE "https://api.numbersprotocol.io/api/v3/assets/$assetNid/" \
